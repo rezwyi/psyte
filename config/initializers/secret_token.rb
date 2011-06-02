9edac24f8f7945b8f7694ad37fe1e2af56d94e10
Sergey Rezvanov <rezwyi@gmail.com>
2011-06-02 23:59:11 +0600

    Added configuration files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twobytes::Application.config.secret_token = '971ed6309c5f910445e1db311630a9a968f878d4f37b4c7545331919bc711cc1c4d815fb2a8a20394c29dcc598f3f43c271ffd82aa8a1bb6ef225cc6002a6f43'
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Twobytes::Application.routes.draw do
+  get 'pages/about'
+  get 'searches/index'
+
+  match 'login', :to => 'sessions#new'
+  match 'logout', :to => 'sessions#destroy'
+
+  resources :sessions
+  resources :posts do
+    get 'manage', :on => :collection
+    get 'feed', :on => :collection, :format => :rss
+  end
+  resources :tags, :only => [:show] do
+    get 'feed', :on => :member, :format => :rss
+  end
+
+  root :to => 'posts#index'
+
+end
