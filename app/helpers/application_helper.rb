fc61a284c3b0b9d79d66c1410d43e28190799dcc
Sergey Rezvanov <rezwyi@gmail.com>
2012-03-05 01:05:53 +0600

    Added helper for copyright years

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(title)
     [title, t(:title)].join(' - ')
   end
+
+  def copyright_years(from = 2011, to = Date.current.year)
+    from != to ? "#{from}-#{to}".html_safe : from.to_s
+  end
 end
