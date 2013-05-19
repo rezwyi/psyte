c47b80bcec87dae1be71f5ff7dc03622ddaa255f
Sergey Rezvanov <sergey@rezvanov.info>
2013-05-19 15:56:47 +0600

    Reorganized assets

--- app/assets/javascripts/all.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require jquery.ui.datepicker
-//= require jquery.ui.datepicker-ru
-//= require jquery.role
-//= require_tree .
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery.role
+//= require jquery.ui.datepicker
+//= require jquery.ui.datepicker-ru
+//= require_directory ./components
+//= require_directory ./plugins
