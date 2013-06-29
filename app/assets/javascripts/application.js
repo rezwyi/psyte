40231b878ee4c9426c7b853aa56d8c18335ff1bd
Sergey Rezvanov <sergey@rezvanov.info>
2013-06-29 16:01:47 +0600

    Install and use flight-for-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,6 @@
 //= require jquery.role
 //= require jquery.ui.datepicker
 //= require jquery.ui.datepicker-ru
+//= require flight-for-rails
 //= require_directory ./components
 //= require_directory ./plugins
